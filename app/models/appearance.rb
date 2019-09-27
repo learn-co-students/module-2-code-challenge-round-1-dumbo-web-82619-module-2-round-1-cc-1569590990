077d936911b912d830ada55b7552794e52975f7c
John Speck <speckworks@gmail.com>
2019-09-27 10:59:58 -0400

    john

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -2,8 +2,8 @@ class Appearance < ApplicationRecord
     has_many :episodes
     has_many :guests
     validates :rating, numericality: {
-        only_integer: true,
-        greater_than: 0, true
+        only_integer: true, 
+        greater_than: 0, true,
         less_than: 5, true
     }
 end
