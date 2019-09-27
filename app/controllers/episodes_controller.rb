876769526a29e23fe353571fe92f83a38b890be7
yukiyao119 <ukiyao@hotmail.com>
2019-09-27 11:10:26 -0400

    deliverables done

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -3,4 +3,15 @@ class EpisodesController < ApplicationController
   def index
     @episodes = Episode.all
   end
+
+  def show
+    @episode = Episode.find(params[:id])
+
+    @all_appearances = @episode.appearances
+
+    @all_guests_arr = @all_appearances.map {|appearance| appearance.guest}
+  end 
+
+
+
 end
