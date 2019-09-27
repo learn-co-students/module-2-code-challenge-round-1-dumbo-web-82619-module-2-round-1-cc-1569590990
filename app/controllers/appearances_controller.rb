bbfb1192b6da925799b4d83fc43ca42914768291
John Speck <speckworks@gmail.com>
2019-09-27 10:48:13 -0400

    my best shot at validations and forms ;O/

--- app/controllers/appearances_controller.rb
+++ app/controllers/appearances_controller.rb
@@ -1,14 +1,16 @@
 class AppearancesController < ApplicationController
   def index
-  end
-
-  def show
+    @appearances = Appearance.all
   end
 
   def new
+    @appearance = Appearance.new
   end
 
   def create
+    @all_the_guests = Guests.all
+    @all_the_episodes = Episodes.all
+    @appearance = Appearance.create(params.require(:appearance).permit(:guest_id, :episode_id, :rating))
   end
 
   def update
--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -1,4 +1,9 @@
 class Appearance < ApplicationRecord
     has_many :episodes
     has_many :guests
+    validates :rating, numericality: {
+        only_integer: true,
+        greater_than: 0, true
+        less_than: 5, true
+    }
 end
