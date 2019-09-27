3942d5fbfde89090af369e1c0fbb06f75d069b7a
Gabriel Kutik <gabrielkutik@yahoo.com>
2019-09-27 11:16:00 -0400

    final

--- app/controllers/appearances_controller.rb
+++ app/controllers/appearances_controller.rb
@@ -1,2 +1,32 @@
 class AppearancesController < ApplicationController
+
+    def index 
+       @appeances = Appearance.all
+    end
+
+    def new     
+       @appearance = Appearance.new
+
+    end
+
+    def create
+        @appearance = Appearance.create(sore)
+        if @appearance.valid?
+            redirect.to epidsode_path(params[:id])
+        else
+            flash[:errors] = @appearance.errors.full_messages
+            redirect_to new_appearance_path
+        end
+    end
+    
+    private
+    def doko
+    Appearance.find_by(params[:id])
+    end
+
+    def sore
+        params.require(:appearance).permit(:guest_id, :episode_id, :rating)
+    end
+
+
 end
--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -1,9 +1,4 @@
 class Appearance < ApplicationRecord
-  belongs_to :guest_id
-  belongs_to :episode_id
-  validates :rating, numericality: { greater_than: 0, less_than_or_equal_to: :max_age }
-
-validates :rating, numericality: { less_than_or_equal_to: 100 }
-
-
+  belongs_to :guest
+  belongs_to :episode
 end
